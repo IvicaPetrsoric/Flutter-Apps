788e95c7384a638b4f265c411596480cfaba041f
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2021-01-16 14:07:23 +0000

    login-ui: setup email and password text fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apps/login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Login UI',
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
