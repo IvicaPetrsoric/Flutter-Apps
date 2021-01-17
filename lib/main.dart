70a9bb2de45876476fcdee7561c27a7f4676f756
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2021-01-17 12:06:49 +0000

    pet-adoption-ui: setup UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apps/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Pet Adoption UI',
+      theme: ThemeData(
+        primaryColor: Color(0xFFFD6456),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
