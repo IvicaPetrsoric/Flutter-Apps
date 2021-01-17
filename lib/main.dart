c3c8984cf8657a56b3a727bc16279fa8c72cd501
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2021-01-17 00:28:07 +0000

    onboarding-ui: setup UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apps/OnboardingScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter onboarding UI',
+      debugShowCheckedModeBanner: false,
+      home: OnboardingScreen(),
+    );
+  }
+}
