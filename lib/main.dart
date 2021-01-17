bd398b54672a12fb8544a8bb2e42c1b511d6673c
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2021-01-17 19:30:27 +0000

    instagram-redesin-ui: setup ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apps/screens/feed_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Instagram Feed UI Redesign',
+      debugShowCheckedModeBanner: false,
+      home: FeedScreen(),
+    );
+  }
+}
