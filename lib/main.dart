f1b3d8e82d8b54606ff016ed1da21567ec9dadab
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2021-01-17 22:19:53 +0000

    notes-ui: setup UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apps/screens/notes_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: NotesScreen(),
+    );
+  }
+}
