0d336ec91c33377f7450f3599e5759b8e55678c1
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2021-01-17 17:41:18 +0000

    menu-options-ui: setup UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apps/screens/menu_options_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Select Option UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.black),
+      home: MenuOptionsScreen(),
+    );
+  }
+}
