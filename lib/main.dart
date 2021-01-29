2450dd995add31b694b7568e6ad5eff81f018c3c
Ivica Petrsoric <ivica.petrsoric@gmail.com>
2021-01-29 17:16:58 +0000

    Game eater: Setup UI/logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_apps/game_controller.dart';
+import 'package:flutter/gestures.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Util flameUtil = Util();
+
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  SharedPreferences storage = await SharedPreferences.getInstance();
+
+  GameController gameController = GameController(storage);
+
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = gameController.onTapDown;
+  flameUtil.addGestureRecognizer(tapper);
+  runApp(gameController.widget);
+
+  // TapGestureRecognizer tapper = TapGestureRecognizer();
+  // tapper.onTapDown = gameController.onTapDown();
+  // flameUtil.addGestureRecognizer(tapper);
+}
